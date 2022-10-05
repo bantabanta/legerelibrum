8c2a2b895a013f0bc5ec59735e05d5dff33fe1b3
Tim Banta <timbanta@me.com>
2022-10-05 16:58:17 -0700

    feat: adds React Router and Links and splits static content into components

---
+++ src/components/SearchBooks.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const SearchBooks = () => {
+
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link to="/"
+          className="close-search"
+        >
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search books by title or author.."
+          />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBooks;
